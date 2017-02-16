c27c39397315946b6ff474664608c8188c10130f
Archi <archi.parikh1@gmail.com>
2017-02-15 20:50:24 -0600

    added code commenting

--- controllers/product.php
+++ controllers/product.php
@@ -1,42 +1,60 @@
 <?php
+/*
+ * Include necessary files.
+ */
 include_once '../models/product_model.php';
+include_once '../models/category_model.php';
 
+/*
+ * Initialize objects.
+ */
 $productModel = new product_model();
-	
+$categoryModel = new category_model();
+
+/*
+ * Initialize variables.
+ */
 $id = (isset($_GET['id']) && $_GET['id'] > 0) ? $_GET['id'] : 0;
 $categoryid = (isset($_GET['CategoryId']) && $_GET['CategoryId'] > 0) ? $_GET['CategoryId'] :0;
 
-//If action, then process(insert, update & delete) data with DB.
-if(isset($_GET['action'])) {	
-	//Upload file into folder.
+/*
+ * If action is set, process data in db.
+ */
+if(isset($_GET['action'])) {
+    /*
+    * Upload file into folder.
+    */
 	$fileName=time()."_".basename($_FILES["image"]["name"]);
 	move_uploaded_file($_FILES["image"]["tmp_name"],"../web/images/".$fileName);
 	
-	//Prepare an array for insert and update.
+	// Prepare an array for insert and update.
 	$postData = array('SKU' =>$_POST['sku'],'Type' =>$_POST['type'],'Price' =>$_POST['price'],'Name' => $_POST['name'],'Image' => $fileName,'CategoryId' => $categoryid);
 	
-	if ($_GET['action'] == 'update'){				
+	if ($_GET['action'] == 'update'){
+        // If action is update and product id is there, update data into db using model function.
 		if($id) {
+            // If image has not been updated, then update existing image from hidden input.
 			if($_FILES["image"]["name"] == '' && $_POST["hiddenimage"] != ''){	
 				$postData['Image'] = $_POST["hiddenimage"];
 			}
 			$productModel->editProduct($id, $postData);			
 		}
+        // If action is insert, insert data into db using model function.
 		else {		
 			$productModel->addProduct($postData);			
 		}
-	} 
+	}
+    // If action is delete, delete data into db using model function.
 	else if ($_GET['action'] == 'delete') {				
 		$productModel->deleteProduct($id);			
 	}
 
-	//Redirect back to view.
+	// Redirect back to view.
 	header('location: ../views/product_view.php?CategoryId='.$categoryid);
 }
 
-//Get the list of products for view.
+/*
+ * collect data form db and create product list array.
+ */
 $productList = $productModel->viewProduct($categoryid);
-
-// display view file's content.
-//echo file_get_contents('http://localhost/website/views/product_view.php');
-	
+$categoryList = $categoryModel->viewCategory();
--- models/product_model.php
+++ models/product_model.php
@@ -1,8 +1,17 @@
 <?php
+/*
+ * Include necessary files.
+ */
 include_once 'connect_db.php';
 
 class product_model {
-	function viewProduct($categoryid) {
+    /*
+     * Run Select Query to get data from table.
+     * Get product data by CategoryId.
+     * @param int $categoyid.
+     * @return array $rows - product rows by categoryid.
+     */
+    function viewProduct($categoryid) {
 	$sql = 'SELECT * FROM product where CategoryId='.$categoryid;
 		$result = mysql_query($sql);
 		
@@ -19,21 +28,37 @@ class product_model {
 			return 0;
 		}
 	}
-	
+
+    /*
+     * Run Insert Query to insert data into table.
+     * @param array $productArray.
+     * @return status of affected rows.
+     */
 	function addProduct($productAray) {
 		$sql = "INSERT INTO product(Name, SKU, Price, Type, Image, CategoryId) VALUES ('".$productAray['Name']."','".$productAray['SKU']."','".$productAray['Price']."','".$productAray['Type']."','".$productAray['Image']."','".$productAray['CategoryId']."')";
 	 	$result = mysql_query($sql);
 		
 		return mysql_affected_rows();
 	}
-	
+
+    /*
+     * Run Update Query to update data into table.
+     * @param int $id.
+     * @param array $productArray.
+     * @return status of affected rows.
+     */
 	function editProduct($id, $productAray) {
 		$sql = "UPDATE product SET Image = '".$productAray['Image']."',Name = '".$productAray['Name']."', SKU = '".$productAray['SKU']."', Type = '".$productAray['Type']."', Price = ".$productAray['Price']." WHERE ProductId= ".$id;
 		$result = mysql_query($sql);
 		
 		return mysql_affected_rows();
 	}
-	
+
+    /*
+     * Run Delete Query to delete data into table.
+     * @param int $id.
+     * @return status of affected rows.
+     */
 	function deleteProduct($id) {
 		$sql = "DELETE FROM product WHERE ProductId=".$id;
 		$result = mysql_query($sql);
