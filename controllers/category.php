2067fc86301a1b5848cd2ff1ff658f0864dd09fe
Archi Parikh <aparikh@norwex.com>
2017-03-19 00:13:46 -0500

    update oops changes

--- controllers/category.php
+++ controllers/category.php
@@ -1,28 +1,46 @@
 <?php
 include_once '../models/category_model.php';
 
-$categoryModel = new category_model();
-	
-$id = (isset($_GET['id']) && $_GET['id'] > 0) ? $_GET['id'] : 0;
-	
-if(isset($_GET['action'])) {	
-	$postData = array('Name' =>$_POST['Name'],'Description' =>$_POST['Description'],'IsActive' => isset($_POST['IsActive'])?1:0);
-	if ($_GET['action'] == 'update'){				
-		if($id) {		
-			$categoryModel->editCategory($id, $postData);			
-		}
-		else {		
-			$categoryModel->addCategory($postData);			
-		}
-	} 
-	else if ($_GET['action'] == 'delete') {				
-		$categoryModel->deleteCategory($id);			
-	}
 
-	header('location: ../views/category_view.php');
+class CategoryController {
+
+    private $categoryModel;
+    public $id;
+
+    /**
+     * CategoryController constructor.
+     */
+    public function __construct()
+    {
+        $this->categoryModel = new category_model();
+        $this->id = (isset($_GET['id']) && $_GET['id'] > 0) ? $_GET['id'] : 0;
+
+        if(isset($_GET['action'])) {
+            $this->postCategory($_POST);
+        }
+    }
+
+    public function getCategory() {
+        $categoryList = $this->categoryModel->viewCategory();
+
+        return $categoryList;
+    }
+
+    public function postCategory($requestData) {
+        $postData = array('Name' =>$requestData['Name'],'Description' =>$requestData['Description'],'IsActive' => isset($requestData['IsActive'])?1:0);
+
+        if ($_GET['action'] == 'update'){
+            if($this->id) {
+                $this->categoryModel->editCategory($this->id, $postData);
+            }
+            else {
+                $this->categoryModel->addCategory($postData);
+            }
+        }
+        else if ($_GET['action'] == 'delete') {
+            $this->categoryModel->deleteCategory($this->id);
+        }
+
+        header('location: ../views/category_view.php');
+    }
 }
-
-$categoryList = $categoryModel->viewCategory();
-// display view file's content.
-//echo file_get_contents('http://localhost/website/views/saree_view.php');
-	
